e1fde16f52403c1deed139706b4816c83a2c224e
Ryan Ardiansyah <ryanardiansyah.person@gmail.com>
2024-01-18 00:28:02 +0700

    component product-list

--- fe/src/components/ProductList.jsx
+++ fe/src/components/ProductList.jsx
@@ -1,7 +1,146 @@
 const ProductList = () => {
     return (
-        <div className="container"></div>
+        <div className="container mx-auto mt-6">
+            <div className="grid gap-y-9 lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-1 justify-items-center">
+                <form class="w-56 font-sans p-3 rounded-lg" style={{ background: "#1E293B" }}>
+                    <div className="  ">
+                        <img src="src/assets/php.jpg" alt="gambar" className="w-full h-full object-cover" loading="lazy" />
+                    </div>
+                    <div className=" items-baseline mb-4 pb-4 border-b border-slate-200">
+                    </div>
+                    <div className=" space-x-4 text-sm font-medium">
+                        <div className="flex-auto flex justify-between">
+                            <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+                                Edit
+                            </button>
+                            <button className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button">
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <form class="w-56 font-sans p-3 rounded-lg" style={{ background: "#1E293B" }}>
+                    <div className="  ">
+                        <img src="src/assets/php.jpg" alt="gambar" className="w-full h-full object-cover" loading="lazy" />
+                    </div>
+                    <div className=" items-baseline mb-4 pb-4 border-b border-slate-200">
+                    </div>
+                    <div className=" space-x-4 text-sm font-medium">
+                        <div className="flex-auto flex justify-between">
+                            <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+                                Edit
+                            </button>
+                            <button className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button">
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <form class="w-56 font-sans p-3 rounded-lg" style={{ background: "#1E293B" }}>
+                    <div className="  ">
+                        <img src="src/assets/php.jpg" alt="gambar" className="w-full h-full object-cover" loading="lazy" />
+                    </div>
+                    <div className=" items-baseline mb-4 pb-4 border-b border-slate-200">
+                    </div>
+                    <div className=" space-x-4 text-sm font-medium">
+                        <div className="flex-auto flex justify-between">
+                            <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+                                Edit
+                            </button>
+                            <button className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button">
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <form class="w-56 font-sans p-3 rounded-lg" style={{ background: "#1E293B" }}>
+                    <div className="  ">
+                        <img src="src/assets/php.jpg" alt="gambar" className="w-full h-full object-cover" loading="lazy" />
+                    </div>
+                    <div className=" items-baseline mb-4 pb-4 border-b border-slate-200">
+                    </div>
+                    <div className=" space-x-4 text-sm font-medium">
+                        <div className="flex-auto flex justify-between">
+                            <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+                                Edit
+                            </button>
+                            <button className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button">
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <form class="w-56 font-sans p-3 rounded-lg" style={{ background: "#1E293B" }}>
+                    <div className="  ">
+                        <img src="src/assets/php.jpg" alt="gambar" className="w-full h-full object-cover" loading="lazy" />
+                    </div>
+                    <div className=" items-baseline mb-4 pb-4 border-b border-slate-200">
+                    </div>
+                    <div className=" space-x-4 text-sm font-medium">
+                        <div className="flex-auto flex justify-between">
+                            <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+                                Edit
+                            </button>
+                            <button className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button">
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <form class="w-56 font-sans p-3 rounded-lg" style={{ background: "#1E293B" }}>
+                    <div className="  ">
+                        <img src="src/assets/php.jpg" alt="gambar" className="w-full h-full object-cover" loading="lazy" />
+                    </div>
+                    <div className=" items-baseline mb-4 pb-4 border-b border-slate-200">
+                    </div>
+                    <div className=" space-x-4 text-sm font-medium">
+                        <div className="flex-auto flex justify-between">
+                            <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+                                Edit
+                            </button>
+                            <button className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button">
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <form class="w-56 font-sans p-3 rounded-lg" style={{ background: "#1E293B" }}>
+                    <div className="  ">
+                        <img src="src/assets/php.jpg" alt="gambar" className="w-full h-full object-cover" loading="lazy" />
+                    </div>
+                    <div className=" items-baseline mb-4 pb-4 border-b border-slate-200">
+                    </div>
+                    <div className=" space-x-4 text-sm font-medium">
+                        <div className="flex-auto flex justify-between">
+                            <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+                                Edit
+                            </button>
+                            <button className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button">
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <form class="w-56 font-sans p-3 rounded-lg" style={{ background: "#1E293B" }}>
+                    <div className="  ">
+                        <img src="src/assets/php.jpg" alt="gambar" className="w-full h-full object-cover" loading="lazy" />
+                    </div>
+                    <div className=" items-baseline mb-4 pb-4 border-b border-slate-200">
+                    </div>
+                    <div className=" space-x-4 text-sm font-medium">
+                        <div className="flex-auto flex justify-between">
+                            <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+                                Edit
+                            </button>
+                            <button className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button">
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
-export default { ProductList }
+export { ProductList }
--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import './index.css'
+import { ProductList } from './components/ProductList.jsx'
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <ProductList></ProductList>
+}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
