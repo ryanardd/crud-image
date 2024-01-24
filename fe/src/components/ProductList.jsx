09f3d5f6257b4059afda398299caa118bfa01d07
Ryan Ardiansyah <ryanardiansyah.person@gmail.com>
2024-01-24 16:21:37 +0700

    style product list

--- fe/src/components/ProductList.jsx
+++ fe/src/components/ProductList.jsx
@@ -9,8 +9,8 @@ import {
     TableHeader,
     TableRow,
 } from "./ui/table"
-import axios from "axios";
 import { deleteProduct, getProduct } from "../services/product.service";
+import { Link } from "react-router-dom";
 
 
 const ProductList = () => {
@@ -34,15 +34,16 @@ const ProductList = () => {
 
     return (
 
-        <div className="container mx-auto mt-6 font-mono">
+        <div className="container w-[85%] mx-auto mt-6 font-mono">
             <h2 className="font-sans font-bold text-3xl my-6">Product List</h2>
             <div className="my-3">
-                <Button className="bg-blue-600">Add Product</Button>
+                <Link to="/add">
+                    <Button className="bg-blue-600">Add Product</Button>
+                </Link>
             </div>
             <Table>
-                <TableCaption>A list of your recent invoices.</TableCaption>
                 <TableHeader>
-                    <TableRow className="bg-slate-400 hover:bg-slate-400">
+                    <TableRow className="bg-slate-400 hover:bg-slate-400 ">
                         <TableHead className="w-[5%] text-center">No.</TableHead>
                         <TableHead className="w-[40%] text-center">Name</TableHead>
                         <TableHead className="w-[30%]">Image</TableHead>
@@ -55,8 +56,10 @@ const ProductList = () => {
                             <TableCell className="font-medium text-center">{index + 1}</TableCell>
                             <TableCell className="text-center">{product.name}</TableCell>
                             <TableCell >
-                                <div className="w-56 h-36">
-                                    <img src={product.url} alt="image" className="w-full h-full object-contain" loading="lazy" />
+                                <div className="w-36 h-16">
+                                    <Link to={product.url} target="_blank">
+                                        <img src={product.url} alt="image" className="w-full h-full object-contain" loading="lazy" />
+                                    </Link>
                                 </div>
                             </TableCell>
                             <TableCell className="text-center">
